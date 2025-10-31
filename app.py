ef99f86dbc93a9364d00621f55e86c37e13dd428
narahari <naraharianandeshi@gmail.com>
2025-10-31 15:08:54 +0530

    more practice done

--- app.py
+++ app.py
@@ -1,4 +1,34 @@
-print("Hello, World!")
-a=303
-b=561
-print(a+b)
+# print("Hello, World!")
+# a=303
+# b=561
+# print(a+b)
+# print(type(a))
+# print(type(b))
+
+# c=True
+# d=2+3
+# print(type(c))
+
+# def app():
+#     a=20
+#     b=30
+#     return print(a+b)
+# app()
+
+age = int(input("Enter your age: "))
+if age>=18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+temperature = int(input("Enter the temperature in Celsius: "))
+if temperature>30:
+    print("It's a hot day.")
+else:
+    print("It's a cold day.")
+
+
+    studentlist = ["Alice", "Bob", "Charlie", "David"]
+
+    for i  in studentlist:
+    print(studentlist) 
